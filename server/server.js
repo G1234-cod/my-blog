00715d9a9c9e19e7828c573b1069794c11de228b
GYX <2671743944@qq.com>
2026-07-07 13:23:44 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,8 @@ app.use(cors({
   }
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const mailLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -44,7 +45,8 @@ const upload = multer({
   storage: storage,
   limits: {
     fileSize: 10 * 1024 * 1024,
-    files: 3
+    files: 3,
+    fieldSize: 50 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
     const allowedTypes = [
